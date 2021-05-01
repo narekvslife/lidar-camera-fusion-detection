5eb05c3a0a6779504654ffb5a0668eae4b954228
Alvandyan <naalvandyan@gmail.com>
2021-05-01 16:57:19 +0300

    Fixed preprocess, now the closesst point in [ring_index, azimuth_bin] cell is taken into account*
    *It is very slowm, need to rewrite it properly, without the double cycle
    .......

--- src/prepocess.py
+++ src/prepocess.py
@@ -12,7 +12,7 @@ def sample_to_rangeview(sample: dict,
         Transform sample's Lidar Point Cloud to Range View.
 
         Range View is an image of size H x W x 5,
-        where H - number of lasersint, W - number of discretized azimuth bins.
+        where H - number of lasers, W - number of discretized azimuth bins.
         Each of 5 channels is responsible for a single interesting feature:
         range, height, azimuth, intensity and flag indicating whether there is a point or not
 
@@ -42,9 +42,6 @@ def sample_to_rangeview(sample: dict,
     # distance to the point is one of the interesting features
     points_df['distance'] = (points_df.x ** 2 + points_df.y ** 2 + points_df.z ** 2) ** 1 / 2
 
-    # we do not need this information anymore
-    points_df.drop(['x', 'y'], axis=1, inplace=True)
-
     # We want to divide front 90 degrees into number of bins equal to width
     bin_size = 90 / width
     points_df['azimuth_bin'] = np.digitize(x=points_df['azimuth'].values - 45,  # right-most value is 45
@@ -54,17 +51,47 @@ def sample_to_rangeview(sample: dict,
     # First, we construct 4 channels: height, intensity, aziumth, distance
     image = np.zeros((height, width, 5))
     try:
-        # TODO: this is not the correct way of finding the clossest point!
-        image[:, :, :4] = points_df.groupby(['ring_index', 'azimuth_bin']) \
-            .min() \
-            .unstack().fillna(0).stack().values.reshape((height, width, 4))  # fill with zeros if point is missing
-        # 5th channel - flag whether there is a point or not. If distance == azimuth == ring_index == 0 => no point
+        # create a table with index [ring_index, azimuth_bin]
+        # and values being the distance of a closest point in that group
+        min_dist_cross_tab = pd.crosstab(points_df.azimuth_bin,
+                                         points_df.ring_index,
+                                         values=points_df.distance,
+                                         aggfunc='min')
+        points_features = []
+
+        for ring_index in range(0, height):
+            for azimuth_bin in range(1, width + 1):
+
+                dist_value = min_dist_cross_tab[ring_index][azimuth_bin]
+                if np.isnan(dist_value):
+                    # if there were no points in [ring_index, azimuth_bin] group, we fill feature values with 0
+                    feature_values = np.zeros((1, 6))
+                else:
+                    # here we find the clossest point of a group
+                    mask = (points_df['azimuth_bin'] == azimuth_bin) \
+                           & (points_df['ring_index'] == ring_index) \
+                           & (points_df['distance'] == dist_value)
+                    # if there are multiple values, we take any of them
+                    feature_values = points_df[mask].drop(['ring_index', 'azimuth_bin'], axis=1).values[0]
+
+                points_features.append(feature_values)
+
+        points_features = np.vstack(points_features).reshape((height, width, 6))
+
+        # we will need point coordinates (x, y) later in LaserNet
+        point_coordinates = points_features[:, :, :2]
+
+        # here are the 4 channels: distance, azimuth, reflectance, and height
+        image[:, :, :4] = points_features[:, :, 2:]
+
+        # 5th channel is a flag which shows whether there is a point or not.
+        # If distance == azimuth == ring_index == 0 => no point
         image[:, :, 4] = (image[:, :, :].sum(axis=2) != 0).astype(int)
 
+        assert image.shape[: 2] == point_coordinates.shape[: 2]
         # need to reflect x and y, so it matches camera view
-        image = image[::-1, ::-1, :]
+        image = image[::-1, ::-1, :].T
     except ValueError:
-        print(sample['token'], points_df.groupby(['ring_index', 'azimuth_bin']) \
-              .min() \
-              .unstack().fillna(0).stack().values.shape)
-    return image
+        print(sample['token'])
+
+    return image, point_coordinates
