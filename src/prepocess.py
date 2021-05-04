3e67f8b307c63b5ec4a74c6f22dd323644d5fe34
Alvandyan <naalvandyan@gmail.com>
2021-05-04 17:53:44 +0300

    Stable sample to RV*
    * needs speed up

--- src/prepocess.py
+++ src/prepocess.py
@@ -26,7 +26,6 @@ def sample_to_rangeview(sample: dict,
     raw_points = scan.reshape((-1, 5))
 
     points_df = pd.DataFrame(raw_points, columns=['x', 'y', 'z', 'intensity', 'ring_index'])
-
     points_df['azimuth'] = np.degrees(np.arctan(points_df['x'] / points_df['y']))
 
     # Transform front azimuth to be in range from 0 to 180
@@ -38,29 +37,30 @@ def sample_to_rangeview(sample: dict,
     # We only care about the front 90 degrees
     front_90_mask = (45 < points_df.azimuth) & (points_df.azimuth < 135)
     points_df = points_df[front_90_mask]
+    df_len = len(points_df)
     # Reindex by row number
-    points_df.index = np.arange(len(points_df))
+    points_df.index = np.arange(df_len)
+    # Add adrtificial row
 
     # distance to the point is one of the interesting features
-    points_df['distance'] = np.linalg.norm((points_df.x, points_df.y, points_df.z))
+    points_df['distance'] = (points_df.x ** 2 + points_df.y ** 2 + points_df.z ** 2) ** 1 / 2
 
     # We want to divide front 90 degrees into number of bins equal to width
     bin_size = 90 / width
     points_df['azimuth_bin'] = np.digitize(x=points_df['azimuth'].values - 45,  # right-most value is 45
                                            bins=np.arange(0, 90, bin_size))
 
+    # artificial "point" which will represent values for missing points
+    points_df.loc[df_len] = (0, 0, 0, 0, 0, 0, 0, 1)
+
     # Finally construct the Range View image
     # First, we construct 4 channels: height, intensity, aziumth, distance
     image = np.zeros((height, width, 5))
-    try:
-        # row numbers of points which have minimal distance in their groups
-        idx_min = points_df.groupby(['ring_index', 'azimuth_bin']).distance.idxmin().unstack(fill_value=0).stack().values
-    except ValueError:
-        print(sample['token'])
-        return None, None
-
+    #     try:
+    # row numbers of points which have minimal distance in their groups
+    idx_min = points_df.groupby(['ring_index', 'azimuth_bin']).distance.idxmin().unstack(
+        fill_value=df_len).stack().values
     points_df.drop(['ring_index', 'azimuth_bin'], inplace=True, axis=1)
-
     points_features = points_df.loc[idx_min].values
     points_features = points_features.reshape((height, width, 6))
 
@@ -76,7 +76,9 @@ def sample_to_rangeview(sample: dict,
 
     assert image.shape[: 2] == point_coordinates.shape[: 2]
     # need to reflect x and y, so it matches camera view
-    image = image[::-1, ::-1, :].T
-    point_coordinates = point_coordinates[::-1, ::-1, :].T
+    image = image[::-1, ::-1, :]
+    point_coordinates = point_coordinates[::-1, ::-1, :]
+    #     except ValueError:
+    #         print(sample['token'])
 
     return image, point_coordinates
