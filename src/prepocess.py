93d3147bdadbaed6c8c671c78bc35754f3e3e63c
Alvandyan <naalvandyan@gmail.com>
2021-05-08 14:12:38 +0300

    Corrected abs coordinate extraction during preprocess, added BB and LiDAR point plot in the end

--- src/prepocess.py
+++ src/prepocess.py
@@ -1,23 +1,29 @@
 import numpy as np
 import pandas as pd
 from os.path import join
+from nuscenes.utils.data_classes import LidarPointCloud, Quaternion
 
 from .settings import DATASET_PATH, NUSCENES
-from scipy.spatial.transform import Rotation
 
 
-def compute_absolute_coordinates(ego_translation: np.array,
-                                 ego_rotation_q: np.array,
-                                 relative_coordinates: np.array):
-    assert len(ego_rotation_q) == 4
-    assert len(ego_translation) == 3
+def transform_pcl_to_global(pcl, sample_data):
+    """
+        :params: pcl in sensor coordinate system
 
-    R = Rotation.from_quat(ego_rotation_q).as_matrix()[:2, :2]
-    relative_coordinates_unsq = np.expand_dims(relative_coordinates, 3)
+        :return: pcl in abslute coordinate system
+    """
 
-    #     print(R.shape, relative_coordinates_unsq.shape)
-    absolute_coordinates = ego_translation[:2] + (R @ relative_coordinates_unsq).squeeze(3)
-    return absolute_coordinates
+    # First step: transform the pointcloud to the ego vehicle frame for the timestamp of the sweep.
+    cs_record = NUSCENES.get('calibrated_sensor', sample_data['calibrated_sensor_token'])
+    pcl.rotate(Quaternion(cs_record['rotation']).rotation_matrix)
+    pcl.translate(np.array(cs_record['translation']))
+
+    # Second step: transform from ego to the global frame.
+    poserecord = NUSCENES.get('ego_pose', sample_data['ego_pose_token'])
+    pcl.rotate(Quaternion(poserecord['rotation']).rotation_matrix)
+    pcl.translate(np.array(poserecord['translation']))
+
+    return pcl
 
 
 def sample_to_rangeview(sample: dict,
@@ -35,7 +41,9 @@ def sample_to_rangeview(sample: dict,
     """
 
     sample_data_token = sample['data']['LIDAR_TOP']
+
     my_sample_lidar_data = NUSCENES.get('sample_data', sample_data_token)
+    ego = NUSCENES.get('ego_pose', my_sample_lidar_data['ego_pose_token'])
 
     lidarseg_labels_filename = join(NUSCENES.dataroot,
                                     NUSCENES.get('lidarseg', sample_data_token)['filename'])
@@ -44,12 +52,20 @@ def sample_to_rangeview(sample: dict,
     points_raw = np.fromfile(DATASET_PATH + my_sample_lidar_data["filename"], dtype=np.float32).reshape((-1, 5))
     point_labeles = np.fromfile(lidarseg_labels_filename, dtype=np.uint8)
 
-    points_df = pd.DataFrame(points_raw, columns=['x', 'y', 'z', 'intensity', 'ring_index'])
+    pcl = LidarPointCloud.from_file(DATASET_PATH + my_sample_lidar_data["filename"])
+    pcl = transform_pcl_to_global(pcl, my_sample_lidar_data)
+
+    points_raw = np.hstack((pcl.points.T[:, :3], points_raw))  # TODO: redo by zeros
+    assert (points_raw.shape[1] == 8)
+
+    points_df = pd.DataFrame(points_raw, columns=['abs_x', 'abs_y', 'abs_z',
+                                                  'rel_x', 'rel_y', 'rel_z',
+                                                  'intensity', 'ring_index'])
     points_df['class'] = point_labeles
-    points_df['azimuth'] = np.degrees(np.arctan(points_df['x'] / points_df['y']))
+    points_df['azimuth'] = np.degrees(np.arctan(points_df['rel_x'] / points_df['rel_y']))
 
     # Transform front azimuth to be in range from 0 to 180
-    mask_front = (points_df.y >= 0)
+    mask_front = (points_df.rel_y >= 0)
     points_df = points_df[mask_front]
     points_df['azimuth'] += 90
     points_df['azimuth'] = 180 - points_df.loc[mask_front, 'azimuth']
@@ -62,7 +78,7 @@ def sample_to_rangeview(sample: dict,
     points_df.index = np.arange(df_len)
 
     # distance to the point is one of the interesting features
-    points_df['distance'] = (points_df.x ** 2 + points_df.y ** 2 + points_df.z ** 2) ** 1 / 2
+    points_df['distance'] = (points_df.rel_x ** 2 + points_df.rel_y ** 2 + points_df.rel_z ** 2) ** 1 / 2
 
     # We want to divide front 90 degrees into number of bins equal to width
     bin_size = 90 / width
@@ -70,42 +86,44 @@ def sample_to_rangeview(sample: dict,
                                            bins=np.arange(0, 90, bin_size))
 
     # artificial "point" which will represent values for missing points
-    points_df.loc[df_len] = (0, 0, 0, 0, 0, 0, 0, 0, 1)
+    points_df.loc[df_len] = (points_df.abs_x.values[0],
+                             points_df.abs_y.values[0],
+                             points_df.abs_z.values[0], 0, 0, 0, 0, 0, 0, 0, 0, 1)
 
     # Finally construct the Range View image
     # First, we construct 4 channels: height, intensity, aziumth, distance
     image = np.zeros((height, width, 5))
-    try:
-        # we only leave the points which have minimal distance in their groups
-        idx_min = points_df.groupby(['ring_index', 'azimuth_bin']) \
-            .distance.idxmin() \
-            .unstack(fill_value=df_len).stack().values
-        points_df = points_df.loc[idx_min]
+    #     try:
+    # row numbers of points which have minimal distance in their groups
 
-        point_labeles = points_df['class'].values.reshape((height, width))
+    idx_min = points_df.groupby(['ring_index', 'azimuth_bin']) \
+        .distance.idxmin() \
+        .unstack(fill_value=df_len).stack().values
+    points_df = points_df.loc[idx_min]
 
-        points_df.drop(['ring_index', 'azimuth_bin', 'class'], inplace=True, axis=1)
+    point_labeles = points_df['class'].values.reshape((height, width))
 
-        points_features = points_df.values.reshape((height, width, 6))
+    points_df.drop(['class'], inplace=True, axis=1)
+    points_df.drop(['ring_index', 'azimuth_bin'], inplace=True, axis=1)
 
-        # during LaserNet inference, we will need absolute coordinates of a point in each cell
-        relative_point_coordinates = points_features[:, :, :2]
-        ego = NUSCENES.get('ego_pose', my_sample_lidar_data['ego_pose_token'])
-        absolute_coordinates = compute_absolute_coordinates(ego_translation=ego['translation'],
-                                                            ego_rotation_q=ego['rotation'],
-                                                            relative_coordinates=relative_point_coordinates)
+    points_features = points_df.values.reshape((height, width, 9))
 
-        # here are the 4 channels: distance, azimuth, reflectance, and height
-        image[:, :, :4] = points_features[:, :, 2:]
+    # first 3 columns were abs_x, abs_y, abs_z
+    absolute_coordinates = np.zeros((height, width, 3))
+    absolute_coordinates[:, :, :2] = points_features[:, :, :2]
 
-        # 5th channel is a flag which shows whether there is a point or not.
-        # If distance == azimuth == ring_index == 0 => no point
-        image[:, :, 4] = (image[:, :, :].sum(axis=2) != 0).astype(int)
+    # here are the 4 channels: distance, azimuth, reflectance, and height
+    image[:, :, :4] = points_features[:, :, 5:]
 
-        assert image.shape[: 2] == absolute_coordinates.shape[: 2]
+    # 5th channel is a flag which shows whether there is a point or not.
+    # If distance == azimuth == ring_index == 0 => no point
+    image[:, :, 4] = (image[:, :, :].sum(axis=2) != 0).astype(int)
 
-    except ValueError:
-        print(sample['token'])
+    assert image.shape[: 2] == absolute_coordinates.shape[: 2]
+
+    #     except ValueError:
+    #         print(sample['token'])
 
     # need to reflect x and y, so it matches camera view
-    return image[::-1, ::-1, :], absolute_coordinates[::-1, ::-1, :], point_labeles[::-1, ::-1]
+    return image[::-1, ::-1, :], absolute_coordinates[::-1, ::-1, ::], point_labeles[::-1, ::-1]
+
