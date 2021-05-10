0113e3f2483af4fd956c78f340f30098a27ba6e2
Alvandyan <naalvandyan@gmail.com>
2021-05-11 00:49:20 +0300

    Addded bb extraction and moved to relative(sensor frame) coordinates in LaserNet

--- src/prepocess.py
+++ src/prepocess.py
@@ -1,29 +1,9 @@
 import numpy as np
 import pandas as pd
 from os.path import join
-from nuscenes.utils.data_classes import LidarPointCloud, Quaternion
+from pyquaternion import Quaternion
 
 from .settings import DATASET_PATH, NUSCENES
-
-
-def transform_pcl_to_global(pcl, sample_data):
-    """
-        :params: pcl in sensor coordinate system
-
-        :return: pcl in abslute coordinate system
-    """
-
-    # First step: transform the pointcloud to the ego vehicle frame
-    cs_record = NUSCENES.get('calibrated_sensor', sample_data['calibrated_sensor_token'])
-    pcl.rotate(Quaternion(cs_record['rotation']).rotation_matrix)
-    pcl.translate(np.array(cs_record['translation']))
-
-    # Second step: transform from ego to the global frame.
-    poserecord = NUSCENES.get('ego_pose', sample_data['ego_pose_token'])
-    pcl.rotate(Quaternion(poserecord['rotation']).rotation_matrix)
-    pcl.translate(np.array(poserecord['translation']))
-
-    return pcl
 
 
 def sample_to_rangeview(sample: dict,
@@ -49,25 +29,19 @@ def sample_to_rangeview(sample: dict,
 
     # loading directly from files to perceive the ring_index information
     points_raw = np.fromfile(DATASET_PATH + my_sample_lidar_data["filename"], dtype=np.float32).reshape((-1, 5))
-    point_labeles = np.fromfile(lidarseg_labels_filename, dtype=np.uint8)
+    point_labels = np.fromfile(lidarseg_labels_filename, dtype=np.uint8)
 
-    pcl = LidarPointCloud.from_file(DATASET_PATH + my_sample_lidar_data["filename"])
-    pcl = transform_pcl_to_global(pcl, my_sample_lidar_data)
+    assert (points_raw.shape[1] == 5)
 
-    points_raw = np.hstack((pcl.points.T[:, :3], points_raw))  # TODO: redo by zeros
-    assert (points_raw.shape[1] == 8)
-
-    points_df = pd.DataFrame(points_raw, columns=['abs_x', 'abs_y', 'abs_z',
-                                                  'rel_x', 'rel_y', 'rel_z',
+    points_df = pd.DataFrame(points_raw, columns=['x', 'y', 'z',
                                                   'intensity', 'ring_index'])
-    points_df['class'] = point_labeles
-    points_df['azimuth'] = np.degrees(np.arctan(points_df['rel_x'] / points_df['rel_y']))
+    points_df['class'] = point_labels
+    points_df['azimuth'] = np.degrees(np.arctan(points_df['x'] / points_df['y']))
 
     # Transform front azimuth to be in range from 0 to 180
-    mask_front = (points_df.rel_y >= 0)
+    mask_front = (points_df.y >= 0)
     points_df = points_df[mask_front]
-    points_df['azimuth'] += 90
-    points_df['azimuth'] = 180 - points_df.loc[mask_front, 'azimuth']
+    points_df['azimuth'] = 90 - points_df.loc[mask_front, 'azimuth']
 
     # We only care about the front 90 degrees
     front_90_mask = (45 < points_df.azimuth) & (points_df.azimuth < 135)
@@ -77,9 +51,9 @@ def sample_to_rangeview(sample: dict,
     points_df.index = np.arange(df_len)
 
     # distance to the point is one of the interesting features
-    points_df['distance'] = np.linalg.norm(np.vstack((points_df.rel_x.values,
-                                                      points_df.rel_y.values,
-                                                      points_df.rel_z.values)), axis=0)
+    points_df['distance'] = np.linalg.norm(np.vstack((points_df.x.values,
+                                                      points_df.y.values,
+                                                      points_df.z.values)), axis=0)
 
     # We want to divide front 90 degrees into number of bins equal to width
     bin_size = 90 / width
@@ -87,9 +61,7 @@ def sample_to_rangeview(sample: dict,
                                            bins=np.arange(0, 90, bin_size))
 
     # artificial "point" which will represent values for missing points
-    points_df.loc[df_len] = (points_df.abs_x.values[0],
-                             points_df.abs_y.values[0],
-                             points_df.abs_z.values[0], 0, 0, 0, 0, 0, 0, 0, 0, 1)
+    points_df.loc[df_len] = (0, 0, 0, 0, 0, 0, 0, 0, 1)
 
     # Finally construct the Range View image
     # First, we construct 4 channels: height, intensity, aziumth, distance
@@ -102,28 +74,27 @@ def sample_to_rangeview(sample: dict,
         .unstack(fill_value=df_len).stack().values
     points_df = points_df.loc[idx_min]
 
-    point_labeles = points_df['class'].values.reshape((height, width))
+    point_labels = points_df['class'].values.reshape((height, width))
 
-    points_df.drop(['class'], inplace=True, axis=1)
-    points_df.drop(['ring_index', 'azimuth_bin'], inplace=True, axis=1)
+    points_df.drop(['class', 'ring_index', 'azimuth_bin'], inplace=True, axis=1)
 
-    points_features = points_df.values.reshape((height, width, 9))
+    points_features = points_df.values.reshape((height, width, 6))
 
     # first 3 columns were abs_x, abs_y, abs_z
-    absolute_coordinates = np.zeros((height, width, 3))
-    absolute_coordinates[:, :, :2] = points_features[:, :, :2]
+    point_coordinates = np.zeros((height, width, 3))
+    point_coordinates[:, :, :2] = points_features[:, :, :2]
 
     # here are the 4 channels: distance, azimuth, reflectance, and height
-    image[:, :, :4] = points_features[:, :, 5:]
+    image[:, :, :4] = points_features[:, :, 2:]
 
     # 5th channel is a flag which shows whether there is a point or not.
     # If distance == azimuth == ring_index == 0 => no point
     image[:, :, 4] = (image[:, :, :].sum(axis=2) != 0).astype(int)
 
-    assert image.shape[: 2] == absolute_coordinates.shape[: 2]
+    assert image.shape[: 2] == point_coordinates.shape[: 2]
 
     #     except ValueError:
     #         print(sample['token'])
 
     # need to reflect x and y, so it matches camera view
-    return image[::-1, ::-1, :], absolute_coordinates[::-1, ::-1, ::], point_labeles[::-1, ::-1]
+    return image[::-1, ::-1, :], point_coordinates[::-1, ::-1, ::], point_labels[::-1, ::-1]
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,8 @@
 import torch
+import numpy as np
+from pyquaternion import Quaternion
+
+from .settings import NUSCENES
 
 
 def rotation_matrix(angles: torch.Tensor):
@@ -18,114 +22,161 @@ def rotation_matrix(angles: torch.Tensor):
     return s3
 
 
-def params_to_abs_center(point_centers: torch.Tensor,
-                         absolute_coordinates: torch.Tensor,
-                         angles: torch.Tensor) -> torch.Tensor:  # 1
+def params_to_coordinates(point_center_params: torch.Tensor,
+                          point_coordinates: torch.Tensor,
+                          angles: torch.Tensor) -> torch.Tensor:  # 1
     """
-        this function converts relative bounding box center to absolute coordinates
+        this function converts relative bounding box parameters to absolute coordinates
 
-        for this we need the absolute coordinates of the ego, and the ego rotation
-
-    :param point_centers:
-    :param absolute_coordinates:
+    :param point_center_params:
+    :param point_coordinates:
     :param angles:
     :return:
     """
 
-    rotation_matrices = rotation_matrix(angles)  # torch.Size([2, 2, 10, 256, 32])
-    rotation_matrices = rotation_matrices.permute(2, 4, 3, 0, 1)  # torch.Size([10, 32, 256, 2, 2])
+    rotation_matrices = rotation_matrix(angles)  # torch.Size([2, 2, N, 256, 32])
+    rotation_matrices = rotation_matrices.permute(2, 4, 3, 0, 1)  # torch.Size([N, 32, 256, 2, 2])
 
-    #     print(rotation_matrices.shape, centerX_centerY.shape, coordinates.shape)
-    #     torch.Size([2, 2, 10, 256, 32]) torch.Size([10, 2, 256, 32]) torch.Size([10, 2, 256, 32])
+#     print(rotation_matrices.shape, centerX_centerY.shape, coordinates.shape)
+#     torch.Size([2, 2, 10, 256, 32]) torch.Size([10, 2, 256, 32]) torch.Size([N, 2, 256, 32])
 
-    point_centers = point_centers.permute(0, 3, 2, 1).unsqueeze(4)  # TODO:
-    absolute_coordinates = absolute_coordinates.permute(0, 3, 2, 1).unsqueeze(4)  # TODO:
+    point_center_params = point_center_params.permute(0, 3, 2, 1).unsqueeze(4)  # TODO:
+    point_coordinates = point_coordinates.permute(0, 3, 2, 1).unsqueeze(4)  # TODO:
 
-    #     torch.Size([2, 2, 10, 256, 32]) torch.Size([10, 32, 256, 2, 1]) torch.Size([10, 32, 256, 2, 1])
-    #     print("1| AC", absolute_coordinates.shape, "RM", rotation_matrices.shape, "point_centers", point_centers.shape)
-    abs_coords = absolute_coordinates + rotation_matrices @ point_centers
+#     torch.Size([2, 2, N, 256, 32]) torch.Size([N, 32, 256, 2, 1]) torch.Size([N, 32, 256, 2, 1])
+#     print("1| AC", absolute_coordinates.shape, "RM", rotation_matrices.shape, "point_centers", point_centers.shape)
+    abs_coords = point_coordinates + rotation_matrices @ point_center_params
 
-    return abs_coords.squeeze(4).permute(0, 3, 2, 1)  # TODO: transformation to separate function
+    return abs_coords.squeeze(4).permute(0, 3, 2, 1)  # TODO:
 
 
-def abs_params_to_corners(bb_abs_center_coords: torch.Tensor,
-                          bb_abs_orientation: torch.Tensor,
-                          lengths: torch.Tensor,
-                          widths: torch.Tensor) -> torch.Tensor:  # 2
+def params_to_corners(bb_center_coords: torch.Tensor,
+                      bb_orientations: torch.Tensor,
+                      lengths: torch.Tensor,
+                      widths: torch.Tensor) -> torch.Tensor:  # 2
     """
 
-    :param bb_abs_center_coords:
-    :param bb_abs_orientation:
+    :param bb_center_coords:
+    :param bb_orientations:
     :param lengths:
     :param widths:
     :return:
     """
-    R = rotation_matrix(bb_abs_orientation).permute(2, 3, 4, 0, 1)
+    R = rotation_matrix(bb_orientations).permute(2, 3, 4, 0, 1)
 
-    #     print("ROTATION_MATRIX:", R.shape, "lw:", torch.stack((lengths,widths)).unsqueeze(4).permute(1, 2, 3, 0, 4).shape)
+#     print("ROTATION_MATRIX:", R.shape, "lw:", torch.stack((lengths,widths)).unsqueeze(4).permute(1, 2, 3, 0, 4).shape)
 
     b1 = R @ torch.stack((lengths, widths)).unsqueeze(4).permute(1, 2, 3, 0, 4)  # TODO:
     b2 = R @ torch.stack((lengths, -widths)).unsqueeze(4).permute(1, 2, 3, 0, 4)  # TODO:
     b3 = R @ torch.stack((-lengths, -widths)).unsqueeze(4).permute(1, 2, 3, 0, 4)  # TODO:
     b4 = R @ torch.stack((-lengths, widths)).unsqueeze(4).permute(1, 2, 3, 0, 4)  # TODO:
 
-    #     print("bb_abs_center_coords:", bb_abs_center_coords.shape, "bn:", b1.squeeze(4).permute(0, 3, 1, 2).shape)
+#     print("bb_abs_center_coords:", bb_abs_center_coords.shape, "bn:", b1.squeeze(4).permute(0, 3, 1, 2).shape)
 
-    b1 = bb_abs_center_coords + b1.squeeze(4).permute(0, 3, 1, 2)  # TODO:
-    b2 = bb_abs_center_coords + b2.squeeze(4).permute(0, 3, 1, 2)  # TODO:
-    b3 = bb_abs_center_coords + b3.squeeze(4).permute(0, 3, 1, 2)  # TODO:
-    b4 = bb_abs_center_coords + b4.squeeze(4).permute(0, 3, 1, 2)  # TODO:
+    b1 = bb_center_coords + b1.squeeze(4).permute(0, 3, 1, 2)  # TODO:
+    b2 = bb_center_coords + b2.squeeze(4).permute(0, 3, 1, 2)  # TODO:
+    b3 = bb_center_coords + b3.squeeze(4).permute(0, 3, 1, 2)  # TODO:
+    b4 = bb_center_coords + b4.squeeze(4).permute(0, 3, 1, 2)  # TODO:
 
     b = torch.hstack((b1, b2, b3, b4)) / 2
 
     return b
 
 
-def rel_params_to_abs_box_corners(bb_params: torch.Tensor,
-                                  absolute_coordinates: torch.Tensor,
-                                  angles: torch.Tensor,
-                                  K: int) -> torch.Tensor:
+def params_to_box_corners(bb_params: torch.Tensor,
+                          point_coordinates: torch.Tensor,
+                          angles: torch.Tensor) -> torch.Tensor:
     """
         This function turns relative predicted bounding box parameters
         into 4 coordinates of a box in an absolute coordinate system
 
-    :param bb_params: tensor of size [N, K * 6, RV_WIDTH, RV_HEIGHT],
+    :param bb_params: tensor of size [N, 6, RV_WIDTH, RV_HEIGHT],
                       6 components are  [d_x, d_y, w_x, w_y, length, width]
-    :param absolute_coordinates:
+    :param point_coordinates: point coordinates in vehicle ego space
     :param angles:
-    :return: tensor of size    [N, K * 8, RV_WIDTH, RV_HEIGHT]
+    :return: tensor of size    [N, 8, RV_WIDTH, RV_HEIGHT]
     """
 
-    #   bb_params.shape  === [10, 18, 128, 32]
+    (point_centers_xy,
+     w_x, w_y,
+     lengths, widths) = (bb_params[:, :2],
+                         bb_params[:, 2], bb_params[:, 3],
+                         bb_params[:, 4], bb_params[:, 5])
 
-    bb_param_number = 6
-    N, _, RV_WIDTH, RV_HEIGHT = bb_params.shape
+    # TODO: to class(?)
+    bb_abs_center_coords = params_to_coordinates(point_centers_xy, point_coordinates, angles)
 
-    bb_abs_corners = torch.zeros((N, K * 8, RV_WIDTH, RV_HEIGHT))
+    bb_abs_orientation = angles + torch.atan2(w_y, w_x)
 
-    #     print("KKKKKKKKKK", K)
-    for k in range(K):
-        index_margin = k * bb_param_number
+    bb_abs_corners = params_to_corners(bb_abs_center_coords,
+                                       bb_abs_orientation,
+                                       lengths,
+                                       widths)
 
-        (centerX_centerY,
-         w_x, w_y,
-         lenghts, widths) = (bb_params[:, index_margin: index_margin + 2],
-                             bb_params[:, index_margin + 2], bb_params[:, index_margin + 3],
-                             bb_params[:, index_margin + 4], bb_params[:, index_margin + 5])
-
-        # getting bb centers in absolute coordinates
-
-        #         print("0|", centerX_centerY.shape, absolute_coordinates.shape, angles.shape)
-        bb_abs_center_coords = params_to_abs_center(centerX_centerY, absolute_coordinates, angles)
-
-        bb_abs_orientation = angles + torch.atan2(w_y, w_x)
-
-        bb_abs_corners_K = abs_params_to_corners(bb_abs_center_coords,
-                                                 bb_abs_orientation,
-                                                 lenghts,
-                                                 widths)
-        #         torch.Size([10, 8, 128, 32]) torch.Size([10, 128, 32, 4, 2])
-
-        #         print("INDEX MARGIN", index_margin)
-        bb_abs_corners[:, k * 8: (k + 1) * 8, :, :] = bb_abs_corners_K
     return bb_abs_corners
+
+
+def point_is_in_front(point_azimuth: int, front_degrees: int = 90):
+    """
+    :param point_azimuth: azimuth of a point
+    :param front_degrees: range of front degrees, in which the point should fall
+    :return:
+    """
+    assert 0 <= front_degrees <= 180
+
+    half_degree_range = front_degrees / 2
+    return 90 - half_degree_range / 2 < point_azimuth  < 90 + half_degree_range
+
+
+from nuscenes.utils.data_classes import Box
+
+
+def get_front_bb(sample: dict):
+    """
+    Function computes and returns
+    An array of points points of a bounding box in sensor coordinates.
+    Each point is a (x, y) coordinate, each BB is 4 points
+
+    :param sample: nuscenes sample dictionary
+    :return: np.array of shape (N, 8, 3)
+    """
+    my_sample_lidar_data = NUSCENES.get('sample_data', sample['data']['LIDAR_TOP'])
+    sample_annotation = NUSCENES.get_boxes(my_sample_lidar_data['token'])
+
+    ego_record = NUSCENES.get('ego_pose', my_sample_lidar_data['ego_pose_token'])
+    cs_record = NUSCENES.get('calibrated_sensor', my_sample_lidar_data['calibrated_sensor_token'])
+
+    # first step: transform from absolute to ego
+    ego_translation = -np.array(ego_record['translation'])
+
+    # second step: transform from ego to sensor
+    cs_translation = -np.array(cs_record['translation'])
+
+    corners = []
+    for box in sample_annotation:
+
+        box.translate(ego_translation)
+        box.rotate(Quaternion(ego_record['rotation']).inverse)
+
+        box.translate(cs_translation)
+        box.rotate(Quaternion(cs_record['rotation']).inverse)
+
+        # at this point bounding boxes are in sensor coordinate system
+        # now we want to exclude such BB that do not have their center
+        # lying in the front 90 degrees
+
+        if box.center[1] <= 0:
+            continue
+
+        box.center_azimuth = np.degrees(np.arctan(box.center[0] / box.center[1]))
+
+        # Transform front azimuth to be in range from 0 to 180
+        box.center_azimuth = 90 - box.center_azimuth
+        if not (45 < box.center_azimuth < 135):
+            continue
+
+        corners.append(box.bottom_corners())
+
+    corners = np.array(corners)
+    corners[:, 2, :] = np.zeros_like(corners[:, 1, :])
+    return corners
